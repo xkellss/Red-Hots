3ce0cdc3cf0fc334add1f7ff4013573855a743d0
Kels <104536953+xkellss@users.noreply.github.com>
2023-03-28 21:22:23 -0500

    Modifiers work
    Modifiers show up in order summary, still need to add functions for buttons.

--- my-app/src/components/menu/CartItem.js
+++ my-app/src/components/menu/CartItem.js
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import CartContext from "../../store/Cart-Context";
 import classes from "./CartItem.module.css";
 import Card from "../ui/Card";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 function CartItem(props) {
     const cartCtx = useContext(CartContext);
@@ -23,11 +25,17 @@ function CartItem(props) {
             <Card>
                 <div className={classes.content}>
                     <h3>{props.item.name}</h3>
-                <span>${props.item.price}</span>
+                    <span>{props.item.modifiers.join(", ")}</span>
+                     <div>${props.item.price}</div>
                 </div>
                 <div className={classes.cartButtons}>
-                    <button className={classes.btnQty} onClick={removeItem} >Remove</button>
+
+                    <button className={classes.buttonRemove} onClick={removeItem}>
+                        <FontAwesomeIcon icon={faTrashAlt} style={{ color: '#f00a0a' }} />
+                    </button>
+
                     <button className={classes.btnQty} onClick={decrementItem} >-</button>
+
                     <button className={classes.btnQty} onClick={incrementItem} >+</button>
                 </div>
             </Card>
